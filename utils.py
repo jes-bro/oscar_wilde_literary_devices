22954acfa931b1ecbcb9376287228d5a3d5a8194
Jessica Brown <jes@ericbrown.net>
2023-03-25 00:03:54 -0400

    Add pop words plotting function

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import nltk
 
 
-def get_data_from_book(url, book_title):
+def get_data_from_book(url, book_title, num_data_pts):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
     text = soup.get_text()
@@ -15,7 +15,11 @@ def get_data_from_book(url, book_title):
         lowered.append(word.lower())
 
     with open(f"{book_title}.csv", "w") as f:
-        f.write(" ".join(words))
-    # text = nltk.text(lowered)
-    # fdist = nltk.FreqDist(text)
-    # fdist.plot(n)
+        f.write(" ".join(lowered))
+    return lowered
+
+
+def plot_most_freq_words(lowered, num_data_pts):
+    text = nltk.Text(lowered)
+    f_dist = nltk.FreqDist(lowered)
+    f_dist.plot(num_data_pts)
