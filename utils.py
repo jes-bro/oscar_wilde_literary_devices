0b0cf662421269ce91f4d50dc96268c307f9cc3d
Lauren Nalajala <lnalajala@olin.edu>
2023-03-26 03:41:22 -0400

    Made edits to utils.py so that get_data_from_book creates a txt file rather than an html files.
    Created commons.csv, which has the top 100 most used words in English.
    
    Created testing.ipynb, where I tried to remove the top 100 most used
    words from the list.

--- utils.py
+++ utils.py
@@ -5,21 +5,23 @@ import matplotlib.pyplot as plt
 import nltk
 
 
-def get_data_from_book(url, book_title, num_data_pts):
+def get_data_from_book(url, book_title):
     response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    text = soup.get_text()
-    words = re.findall("\w+", text)
+    response.encoding = "UTF-8"
+    response_text = response.text
+    response_words = response_text.replace("\ufeff", "")
+    words = response_words.split()
+
     lowered = []
     for word in words:
         lowered.append(word.lower())
 
-    with open(f"{book_title}.csv", "w") as f:
-        f.write(" ".join(lowered))
+    with open(f"{book_title}.txt", "w") as f:
+        for i in lowered:
+            f.write(f"{i} ")
     return lowered
 
 
 def plot_most_freq_words(lowered, num_data_pts):
-    text = nltk.Text(lowered)
     f_dist = nltk.FreqDist(lowered)
     f_dist.plot(num_data_pts)
