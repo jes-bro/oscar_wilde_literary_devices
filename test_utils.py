fe6808f6c5091063d235c09106c0861c89367bf2
Jessica Brown <jes@ericbrown.net>
2023-03-29 22:11:54 -0400

    Fix unit testing issue

--- test_utils.py
+++ test_utils.py
@@ -1,23 +1,30 @@
 """
-Test the plotting functions in utils.py
+Test the text analysis functions in utils.py
 """
 
-import unittest
 from utils import plot_most_freq_words_texts
 import pytest
 
 
 list_of_urls = [("fake_url", "test_txt1")]
 plot_most_freq_words_texts_cases = [
-    # Check that the complement of A is T.
+    # Check that function correctly counts words
+    # in a text file with one sentence and 2 words
+    # repeated word is consecutive
     (([("fake_url", "test_txt1")], 2), {"have": 9, "not": 1}),
+    # check if repeated word is not consecutive
+    # for multiple repeated words
+    (
+        ([("fake_url", "test_txt2")], 2),
+        {"hello": 9, "from": 1, "the": 1, "other": 1, "side": 1},
+    ),
 ]
 
 
 @pytest.mark.parametrize("input_tuple,output_dict", plot_most_freq_words_texts_cases)
 def test_plot_most_freq_words_texts(input_tuple, output_dict):
     """
-    Test that each nucleotide is mapped to its correct complement.
+    Test that each .
 
     Given a single-character string representing a nucleotide that is "A", "T",
     "G", or "C", check that the get_complement function correctly maps the
@@ -30,5 +37,7 @@ def test_plot_most_freq_words_texts(input_tuple, output_dict):
         complement: A single-character string equal to "A", "C", "T", or "G"
             representing the expected complement of nucleotide.
     """
+    print(input_tuple)
+    print(output_dict)
     assert plot_most_freq_words_texts(input_tuple[0], input_tuple[1]) == output_dict
 
